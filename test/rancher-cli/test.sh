87fbf1f46a97d09b160b4be730559eece6365638
Marc Hermans <marc.hermans@ldtteam.com>
2025-08-21 13:56:13 +0200

    Update tests to cover the rancher-cli tool.

--- test/rancher-cli/test.sh
+++ test/rancher-cli/test.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "kubeconform" kubeconform -v
+check "rancher-cli" rancher --version
 
 # Report result
 reportResults
